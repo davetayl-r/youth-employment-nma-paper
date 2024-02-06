30e4e68e5fc71592b52e251fd79c27e6589c262e
Dave Taylor <6992586+davetayl-r@users.noreply.github.com>
2024-02-06 16:24:27 +1100

    PRISMA 2020 figure for publication

--- network-meta/visualisation/code/nma_prisma.R
+++ network-meta/visualisation/code/nma_prisma.R
@@ -1,28 +1,28 @@
-## PRISMA CHART
+## Create PRISMA 2020 figure for publication
 
+# load required packages
 library(tidyverse)
 library(readxl)
 library(PRISMA2020)
 
-## READ DATA
-
-raw_prisma_data_location <- "./visualisation/input/nma_prisma_data.csv"
+# read data
+raw_prisma_data_location <- "./network-meta/visualisation/input/nma_prisma_data.csv"
 raw_prisma_data <- read.csv(raw_prisma_data_location)
-#raw_prisma_data_location <- "./visualisation/input/nma_prisma_data.xlsx"
-#raw_prisma_data <- read_excel(raw_prisma_data_location)
 
-## CREATE PRISMA CHART
-
+# convert raw data to PRISMA chart form
 prisma_data <- PRISMA_data(raw_prisma_data)
 
+# create figure
 prisma_figure <- PRISMA_flowdiagram(
   data = prisma_data,
   interactive = FALSE,
+  #databases = TRUE,
+  detail_databases = TRUE,
   previous = FALSE,
   other = TRUE,
-  fontsize = 12,
+  fontsize = 10,
   font = "Helvetica",
-  title_colour = "Goldenrod1",
+  #title_colour = "Goldenrod1",
   greybox_colour = "Gainsboro",
   main_colour = "Black",
   arrow_colour = "Black",
@@ -30,10 +30,12 @@ prisma_figure <- PRISMA_flowdiagram(
   arrow_tail = "none",
   side_boxes = TRUE
 )
+# view figure
 prisma_figure
 
+# export figure
 PRISMA_save(
   plotobj = prisma_figure, 
-  filename = "./visualisation/output/nma_prisma_flowchart.png",
+  filename = "./network-meta/visualisation/output/nma_prisma_flowchart.png",
   filetype = "png",
   overwrite = TRUE)
